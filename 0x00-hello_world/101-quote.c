b200a08abedf9cd19f3937ffa10e566feff799a0
Your Name <gahboubwafa@gmail.com>
2023-03-16 12:26:49 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints a message with special caracters to the console
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
