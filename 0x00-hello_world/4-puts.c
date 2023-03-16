06d287a566b0756bafda7035ff56a255909deebf
Your Name <gahboubwafa@gmail.com>
2023-03-16 09:04:47 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * /
- int main(void)
- {
-       puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+int main()
+{
+    puts("\" Programming is like building a multilingual puzzle");
+return 0;
 }
