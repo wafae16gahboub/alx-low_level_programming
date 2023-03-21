cb7da967bd2ba07043d0049ea79ebd7de75d2458
Your Name <gahboubwafa@gmail.com>
2023-03-21 07:42:23 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - Prints all lowercase letters of the English alphabet
+* Your name
+* Return: 0
+*/
+void print_alphabet(void)
+{
+char letter;
+
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+
+_putchar('\n');
+}
