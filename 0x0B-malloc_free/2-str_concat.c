0ae5bfa034005bca854e488185fb13df10b9a731
Your Name <gahboubwafa@gmail.com>
2023-04-11 18:58:18 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,30 +10,34 @@ char *str_concat(char *s1, char *s2)
 int i, j, a, b;
 char *z;
 
-for (i = 0; s1[i] != '\0'; i++)
-{
-}
-for (j = 0; s2[j] != '\0'; j++)
-{
-}
-z = malloc(sizeof(char) * (i + j + 1));
-if (z == NULL)
-{
-return (0);
-}
-for (a = 0; a < i; a++)
-{
-z[a] = s1[a];
-}
-for (b = 0; b < j; b++)
-{
-z[a] = s2[b];
-a++;
-}
-if ((s1 == NULL && s2 != NULL) || (s1 != NULL && s2 == NULL))
-{
-z[a] = '\0';
-return (z);
-}
-return (z);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+	z = malloc(sizeof(char) * (i + j + 1));
+	if (z == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < i; a++)
+	{
+		z[a] = s1[a];
+	}
+	for (b = 0; b < j; b++)
+	{
+		z[a] = s2[b];
+		a++;
+	}
+
+	return (z);
 }
