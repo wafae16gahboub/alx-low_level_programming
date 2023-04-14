b1d6bf1d5d613c0a01002c719513591b4f3d7b34
Your Name <gahboubwafa@gmail.com>
2023-04-13 17:26:40 -0700

    1.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,34 +9,33 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int len1, len2, i, j;
+char *str;
+unsigned int len1;
+unsigned int len2;
+unsigned int i;
+unsigned int j;
 
-	if (!s1)
-		s1 = "";
-	if (!s2)
-		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-	str = malloc(sizeof(char) * (i + j + 1));
-	if (!str)
-		return (NULL);
-	for (len1 = 0; len1 < i; len1++)
-	{
-		str[len1] = s1[len1];
-	}
-	for (len2 = 0; len2 < j; len2++)
-	{
-		str[len1] = s2[len2];
-			len1++;
-	}
-	if (n >= j)
-		return (s2);
-	if (!str)
-		return ("");
-	return (str);
+if (!s1)
+s1 = "";
+if (!s2)
+s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+{
+}
+for (j = 0; s2[j] != '\0' && j < n; j++)
+{
+}
+str = malloc(sizeof(char) * (i + j + 1));
+if (!str)
+return (NULL);
+for (len1 = 0; len1 < i; len1++)
+{
+str[len1] = s1[len1];
+}
+for (len2 = 0; len2 < j; len2++)
+{
+str[len1 + len2] = s2[len2];
+}
+str[len1 + len2] = '\0';
+return (str);
 }
