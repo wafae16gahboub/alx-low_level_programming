9ba4365b266d20d2750dfb43ffeffa3c025b8a59
Your Name <gahboubwafa@gmail.com>
2023-04-17 10:32:53 -0700

    0.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_H
+#define dog_H
+/**
+ * struct dog - Define a new type
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+
+	char *owner;
+};
+#endif
