1df01bf3946e7caba385639b321af7a608e21a5d
Your Name <gahboubwafa@gmail.com>
2023-04-19 08:32:40 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - a function that prints a name
+ * @name: name
+ * @f: pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
