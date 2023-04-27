2fb7fdb884ac17e8f54ef3ccf1bb21f3409be9ed
Your Name <gahboubwafa@gmail.com>
2023-04-27 14:36:03 -0700

    5.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * printStatement - a function that prints all
+ * by me
+ * Return: void
+ */
+void printStatement(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
