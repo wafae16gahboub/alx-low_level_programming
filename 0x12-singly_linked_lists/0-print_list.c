170565a46548f1a3a51d4c57313dc3dbe7969170
Your Name <gahboubwafa@gmail.com>
2023-04-27 07:40:28 -0700

    0.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - a function that prints all the elements of a list
+ * @h: pointer
+ * by me
+ * Return: void
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
