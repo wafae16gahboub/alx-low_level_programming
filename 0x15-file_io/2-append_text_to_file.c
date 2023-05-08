988bb3dc09c437e55c58074296bba0a6d1b1555d
Your Name <gahboubwafa@gmail.com>
2023-05-08 16:22:51 -0700

    2.

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int f, r, l;
+	int f, r;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,14 +19,11 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (l = 0; text_content[l]; l++)
-		{
-		r = write(f, text_content, l);
+		r = write(f, text_content, strlen(text_content));
 		if (r == -1)
 		{
 			close(f);
 			return (-1);
-		}
 		}
 	}
 	close(f);
