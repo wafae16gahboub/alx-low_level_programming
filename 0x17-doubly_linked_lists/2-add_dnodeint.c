8655767e8c5933d806e09a377861811d61cc81b4
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-06-15 16:22:21 -0700

    by wafa

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_dnodeint - Adds a new node at the beginning of a doubly linked list
  * @head: Pointer to the head of the list
- * @n: Value 
+ * @n: Value
  * Return: The address of the new element
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
